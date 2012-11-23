ef97fd3ee2a194aa15659d3b50c46f957d6e35ee
Jonathan Wills <runningwild@gmail.com>
2012-11-23 14:41:49 -0800

    Removed net_player

--- make.bash
+++ make.bash
@@ -3,9 +3,6 @@ mkdir -p bin
 # build the barbu runner
 go build -o bin/runner ./runner/
 
-# build the net player binary
-go build -o ./bin/net_player ./net_player/
-
 # build our ais
 go build -o bin/jonai ./jonai
 g++ -O2 -o bin/simpleai ./cc_ai_base/*.cc ./simpleai/*.cc
